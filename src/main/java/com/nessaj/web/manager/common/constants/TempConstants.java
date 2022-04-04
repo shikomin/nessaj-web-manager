febbe946ce8dcfd2fb6116e0a5376c37a0907aab
shikomin <keming_xu@163.com>
2022-04-05 00:03:54 +0800

    [msg]:git ignore

---
+++ src/main/java/com/nessaj/web/manager/common/constants/TempConstants.java
@@ -0,0 +1,16 @@
+package com.nessaj.web.manager.common.constants;
+
+/**
+ * TODO:2022/2/10 写死的信息，日后统一处理配置信息
+ *
+ * @author keming
+ * @Date 2022/02/10 18:59
+ */
+public class TemporaryConstants {
+
+    public static final String MODULE_LOCAL_STORAGE = "E:/Nessaj/local-workspace/manager/packages/";
+    public static final String MODULE_UNZIP_PATH = "E:/Nessaj/local-workspace/manager/modules/";
+    public static final String RIGHT_SEPARATOR = "/";
+    public static final String MODULE_MANIFEST_FILE = "manifest.json";
+
+}
